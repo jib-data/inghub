8ad234ec5da417b6f8a37da1888c6f933bf10db6
Jibril Richter <jibrilrichter@gmail.com>
2024-12-23 23:43:24 -0600

    remove comments

--- frontend/src/Timeline/Post/PostContainer.js
+++ frontend/src/Timeline/Post/PostContainer.js
@@ -1,31 +1,3 @@
-// import { useContext, useState } from "react";
-// import PostCard from "./PostCard";
-// import HomeData from "../TimelineDataContext";
-
-// let PostContainer = () => {
-//   // extract posts
-//   // loop through the list then render
-//   const { posts, setPosts } = useContext(HomeData);
-
-//   return (
-//     <div className="post-container">
-//       <h4 className="post-header">Posts</h4>
-//       <input
-//         type="text"
-//         className="post-search"
-//         placeholder="search posts by author"
-//       />
-//       {/* {filteredPosts.map((post) => {
-//         return <PostCard key={post.postId} post={post} setPosts={setPosts} />;
-//       })} */}
-//       {posts.map((post) => {
-//         return <PostCard key={post.postId} post={post} setPosts={setPosts} />;
-//       })}
-//     </div>
-//   );
-// };
-// export default PostContainer;
-
 import { useContext, useState, useEffect } from "react";
 import PostCard from "./PostCard";
 import HomeData from "../TimelineDataContext";
